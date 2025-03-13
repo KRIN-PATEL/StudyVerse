93b134a59162a3ad7fe5bd5f64f4be63b5d7f63f
Krin Patel <you@example.com>
2025-03-12 23:33:01 -0400

    Added fetch publish course api

--- backend/routes/course.routes.js
+++ backend/routes/course.routes.js
@@ -3,10 +3,11 @@ import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
 
-import { createCourse, createLecture, deleteCourse, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getLectureById, removeLecture, togglePublishCourse } from "../controllers/course.controller.js";
+import { createCourse, createLecture, deleteCourse, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getLectureById, getPublishedCourse, removeLecture, togglePublishCourse } from "../controllers/course.controller.js";
 const router = express.Router();
 // course routing
 router.route("/").post(isAuthenticated, createCourse);
+router.route("/published-courses").get( getPublishedCourse);
 router.route("/").get(isAuthenticated, getCreatorCourses);
 router.route("/:courseId").put(isAuthenticated,upload.single("courseThumbnail"), editCourse);
 router.route("/:courseId").get(isAuthenticated, getCourseById);
