11c83b4b45f711b15cb82b1d35a8f20c6abae3c9
KRIN-PATEL <190770116089@socet.edu.in>
2025-04-06 19:27:35 -0400

    Toast message Update

--- frontend/src/pages/client/Profile.jsx
+++ frontend/src/pages/client/Profile.jsx
@@ -40,7 +40,7 @@ const Profile = () => {
 
   useEffect(() => {
     if (user) {
-      setName(user.name || ""); // Populate form with user data
+      setName(user.name || ""); 
     }
   }, [user]);
 
