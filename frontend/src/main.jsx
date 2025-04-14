0b8fbab72c7ff0712cfcfe7a85020bbb385d3751
KRIN-PATEL <190770116089@socet.edu.in>
2025-04-13 22:51:39 -0400

    protected routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,19 +5,14 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { appStore } from "./app/store";
 import { Toaster } from "sonner";
-import { useLoadUserQuery } from "./features/api/authApi";
-import LoadingSpinner from "./components/LoadingSpinner";
+import CustomLoader from "./components/CustomLoader";
 
-const Custom = ({ children }) => {
-  const { isLoading } = useLoadUserQuery();
-  return <>{isLoading ? <LoadingSpinner/> : <>{children}</>}</>;
-};
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
-    <Custom>
-      <App />
-      </Custom>
+      <CustomLoader>
+        <App />
+      </CustomLoader>
       <Toaster />
     </Provider>
   </StrictMode>
