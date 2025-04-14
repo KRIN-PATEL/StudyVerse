a99de6255c392c308d87b225eb458aab3d397097
KRIN-PATEL <190770116089@socet.edu.in>
2025-04-13 22:50:57 -0400

    solve bug

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -280,21 +280,6 @@ export const getUsers = async (req, res) => {
     });
   }
 };
-// export const getUsers = async (req, res) => {
-//   try {
-//     const users = await User.find().select("-password");
-//     return res.status(200).json({
-//       success: true,
-//       users,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({
-//       success: false,
-//       message: "Failed to fetch users",
-//     });
-//   }
-// };
 
 export const deleteUser = async (req, res) => {
   try {
